5ebdb4a2f0e57916f656a97c9ccd07d161c2dcbe
halcyon.ace <michaelababao200@gmail.com>
2020-05-04 22:44:06 +0800

    added main path

--- personal_profile/urls.py
+++ personal_profile/urls.py
@@ -19,9 +19,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from portfolio import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/', include('portfolio.urls', namespace='portfolio'))
+    path('portfolio/', include('portfolio.urls', namespace='portfolio')),
+    path('', views.homepage)
 ]
 
 if settings.DEBUG:

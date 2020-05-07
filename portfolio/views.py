c6a6f6875ad60daf6c764d6cd05296c72f11a0a0
halcyon.ace <michaelababao200@gmail.com>
2020-05-07 10:42:34 +0800

    add previous in portfolio list

--- portfolio/views.py
+++ portfolio/views.py
@@ -15,10 +15,19 @@ def detail(request, name):
 	project = get_object_or_404(Portfolio, slug=name)
 	template_name = 'portfolio/detail.html'
 	next_project = Portfolio.objects.exclude(pk__lte=project.pk).order_by('id')
+	previous_project = None
+	if project.pk != 1:
+		previous_project = Portfolio.objects.get(pk=(project.pk-1))
 	
 	if not next_project:
 		next_project = None
 	else:
 		next_project = next_project[0]
 
-	return render(request, template_name, {'project': project, 'next_project': next_project})
+	context = {
+		'project': project,
+		'next_project': next_project,
+		'previous_project': previous_project
+	}
+
+	return render(request, template_name, context)

79304bc3d174fd77470b59c90b68a7ef3b0a2a5e
michael.ababao <michaelababao200@gmail.com>
2021-01-02 05:20:31 +0800

    Added project/ on projects path

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,6 @@ app_name = 'portfolio'
 
 urlpatterns = [
     path('', views.HomeListView.as_view(), name='homepage'),
-    path('<slug:slug>/', views.ProjectDetailView.as_view(), name='project_details'),
+    path('project/<slug:slug>/', views.ProjectDetailView.as_view(), name='project_details'),
     path('form/send-message/', views.send_message, name='send_message'),
 ]

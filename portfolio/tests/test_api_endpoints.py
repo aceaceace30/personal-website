002fbaff2d772cbc7857a7e087793458fee6244c
michael.ababao <michaelababao200@gmail.com>
2021-01-17 20:30:32 +0800

    Added Todos for API endpoints

---
+++ portfolio/tests/test_api_endpoints.py
@@ -0,0 +1,23 @@
+from rest_framework.test import APILiveServerTestCase
+
+
+class APIEndpointsTestCase(APILiveServerTestCase):
+    """Tests for API endpoints. Break this down to seperate classes or files if the endpoints grows large"""
+
+    def test_about_endpoint(self):
+        """Todo: Add test for about endpoint"""
+
+    def test_project_endpoint(self):
+        """Todo: Add test for project endpoint"""
+
+    def test_project_detail_endpoint(self):
+        """Todo: Add test for project detail endpoint"""
+
+    def test_skill_endpoint(self):
+        """Todo: Add test for skill endpoint"""
+
+    def test_job_experience_endpoint(self):
+        """Todo: Add test for about endpoint"""
+
+    def test_testimonial_endpoint(self):
+        """Todo: Add test for testimonial endpoint"""

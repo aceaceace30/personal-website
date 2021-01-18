cdaed2c27c5253e7947996ada4414009e723c0c7
michael.ababao <michaelababao200@gmail.com>
2021-01-18 09:39:40 +0800

    Added whitenoise package

--- personal_profile/settings.py
+++ personal_profile/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ]
 
 ROOT_URLCONF = 'personal_profile.urls'

e9b123214859938c7137a068c78a35948ca669f6
michael.ababao <michaelababao200@gmail.com>
2021-01-23 16:24:34 +0800

    Added path for blog-detail

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,5 +6,6 @@ app_name = 'blogs'
 
 
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name='blog-list')
+    path('', views.BlogListView.as_view(), name='blog-list'),
+    path('<slug:slug>/', views.BlogDetailView.as_view(), name='blog-detail'),
 ]

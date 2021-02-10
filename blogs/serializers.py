672b3f03db33cb6dfd2c9b4cff4a5d4a171987ab
michael.ababao <michaelababao200@gmail.com>
2021-02-10 19:35:12 +0800

    Added TagSerializer

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from blogs.models import Blog
+from blogs.models import Blog, Tag
 
 
-class BlogSerializer(serializers.Serializer):
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['name', 'color']
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(read_only=True, many=True)
+
     class Meta:
         model = Blog
         fields = ['title', 'slug', 'cover', 'content', 'tags']

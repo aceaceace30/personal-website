3ac18da9311a090cdab459262ee6c116fc219dec
michael.ababao <michaelababao200@gmail.com>
2021-02-10 19:35:29 +0800

    Updated BlogViewSet

--- blogs/api_views.py
+++ blogs/api_views.py
@@ -9,7 +9,7 @@ from blogs.serializers import BlogSerializer
 class BlogViewSet(ModelViewSet):
     """API Endpoint that retrieves the blog posts"""
     serializer_class = BlogSerializer
-    queryset = Blog.objects.filter(active=True)
+    queryset = Blog.objects.prefetch_related('blogtag_set').filter(active=True)
     filter_backends = [OrderingFilter, DjangoFilterBackend]
     ordering_fields = ['title', 'slug']
-    filterset_fields = ['title', 'slug']
+    filterset_fields = ['title', 'slug', 'tags']

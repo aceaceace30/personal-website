eaad9acd13e5f6c1b7952b18d7f89e20182a8c8e
michael.ababao <michaelababao200@gmail.com>
2021-02-11 19:47:54 +0800

    Completed unit test for blog endpoint

--- blogs/tests/test_api_endpoints.py
+++ blogs/tests/test_api_endpoints.py
@@ -9,11 +9,34 @@ class BlogEndpointsTestCase(APILiveServerTestCase):
 
     def test_blog_list(self):
         """Asserts that blog list endpoint is returning correct response"""
-
         url = reverse('api-blog-list')
         response = self.client.get(url)
 
         self.assertEqual(200, response.status_code)
-        self.assertEqual(3, response.data['count'])
+        self.assertEqual(4, response.data['count'])
 
-        # TODO: Added assertion to check if the fields being retrieve is complete
+        # check if each item in results has complete keys
+        for item in response.data['results']:
+            self.assertEqual({'title', 'slug', 'cover', 'content', 'tags'}, set(item.keys()))
+
+    def test_blog_list_filter(self):
+        """Asserts that blog list endpoint is returning correct response with filter"""
+        url = reverse('api-blog-list')
+
+        get_params = [
+            {'title': '10 Git Commands you use every'},
+            {'tags': 'Workout,Python'},
+            {'slug': '10-git-commands-you-use-everyday'},
+            {'tags': 'Workout'}
+        ]
+
+        expected_count = [1, 3, 1, 1]
+
+        for count, get_param in zip(expected_count, get_params):
+            response = self.client.get(url, get_param)
+
+            self.assertEqual(200, response.status_code)
+            self.assertEqual(count, response.data['count'])
+            # check if each item in results has complete keys
+            for item in response.data['results']:
+                self.assertEqual({'title', 'slug', 'cover', 'content', 'tags'}, set(item.keys()))

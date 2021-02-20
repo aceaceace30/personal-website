712a2a40b99f3b369a16729cc2037554fa9a715d
michael.ababao <michaelababao200@gmail.com>
2021-02-20 10:51:32 +0800

    Updated to readonly viewset

--- blogs/api_views.py
+++ blogs/api_views.py
@@ -1,11 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 
 from blogs.filters import BlogFilter
 from blogs.models import Blog
 from blogs.serializers import BlogSerializer
 
 
-class BlogViewSet(ModelViewSet):
+class BlogViewSet(viewsets.ReadOnlyModelViewSet):
     """API Endpoint that retrieves the blog posts"""
     serializer_class = BlogSerializer
     queryset = Blog.objects.prefetch_related('tags')

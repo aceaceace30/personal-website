455812800736e1542e34f40cd36e4de03dc024b1
michael.ababao <michaelababao200@gmail.com>
2021-03-20 17:41:08 +0800

    Add task for checking site broken links

--- portfolio/tasks.py
+++ portfolio/tasks.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 from personal_profile.celery import app
+from portfolio.scraper import LinkChecker
 
 
 @app.task(name='send_mail_task', ignore_result=True)
-def send_mail_task(subject, email, message):
+def send_mail_task(subject: str, email: str, message: str) -> None:
     """Task for sending email"""
     send_mail(
         f'{subject} - {email}',
@@ -14,3 +15,10 @@ def send_mail_task(subject, email, message):
         [settings.EMAIL_ADMIN],
         fail_silently=False
     )
+
+
+@app.task(name='check_site_broken_links', ignore_result=True)
+def check_site_broken_links() -> None:
+    """Task for checking site broken links and sending email notification if there is one"""
+    link_checker = LinkChecker()
+    link_checker.run()

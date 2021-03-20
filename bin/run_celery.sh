3d8c78e4ea3930a7d551f71bff288c45875b6072
michael.ababao <michaelababao200@gmail.com>
2021-03-20 18:09:29 +0800

    Updated sh scripts

--- bin/run_celery.sh
+++ bin/run_celery.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+cd /home/ace/personal-website || exit
+source venv/bin/activate
 exec celery -A personal_website worker -l info --pool=solo
--- bin/run_celery_beat.sh
+++ bin/run_celery_beat.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+cd /home/ace/personal-website || exit
+source venv/bin/activate
 celery -A personal_website beat -l info

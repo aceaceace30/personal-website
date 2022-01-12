732befd7305199de88d53c6262ec4ac7812f0f36
michael.ababao <michaelababao200@gmail.com>
2022-01-12 13:17:26 +0800

    Added s3 media handling

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django_filters',
     'colorfield',
     'django_celery_results',
+    'storages',
 
     # django built-ins
     'django.contrib.admin',
@@ -132,12 +133,25 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# AWS
+AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_QUERYSTRING_AUTH = False  # django-ckeditor will not work with S3 through django-storages without this line
+
+if not DEBUG:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    DEFAULT_FILE_STORAGE = 'personal_website.storage_backends.PublicMediaStorage'
 
 CKEDITOR_UPLOAD_PATH = 'ckeditor/uploads/'
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email settings
 
